63cb12a7129610d50616b2ad0e70e75ec52e2dd3
Sarah Tennis <sarah.n.tennis@gmail.com>
2019-09-18 11:52:09 -0400

    Work on landing page

--- client/src/components/SignIn.js
+++ client/src/components/SignIn.js
@@ -35,7 +35,7 @@ class SignIn extends React.Component {
 
   render() {
     return (
-      <div className="auth-container">
+      <div className={`auth-container ${this.props.hidden ? "hidden" : ""}`}>
         <img src={Logo} alt="logo" />
         <h2>Sign In</h2>
         <table>
