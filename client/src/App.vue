c5e8c8a7aa35b1ff945e7a4d610f48a2d8fd07e4
Sarah Tennis <sarah.n.tennis@gmail.com>
2020-02-16 21:04:13 -0500

    CSS reset

--- client/src/App.vue
+++ client/src/App.vue
@@ -16,6 +16,8 @@ export default {
 </script>
 
 <style lang="scss">
+@import url("./assets/scss/reset.scss");
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
